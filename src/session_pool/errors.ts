715f3979ada2c63aa57eeb69ba3c8ce704380ffb
Martin Adámek <banan23@gmail.com>
2021-10-25 16:33:16 +0200

    remove root src folder in favour of packages folder

--- src/session_pool/errors.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @ignore
- */
-export class CookieParseError extends Error {
-    constructor(readonly cookieHeaderString: unknown) {
-        super(`Could not parse cookie header string: ${cookieHeaderString}`);
-        Error.captureStackTrace(this, CookieParseError);
-    }
-}
--- src/session_pool/events.ts
+++
@@ -1,1 +0,0 @@
-export const EVENT_SESSION_RETIRED = 'sessionRetired';
