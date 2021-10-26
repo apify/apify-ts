8e6093f7f360056445e994395ca55c4d7b29bcd5
Martin Adámek <banan23@gmail.com>
2021-10-26 15:43:38 +0200

    remove unused pages from sdk docs (users, help), use default HP for now

--- website/docusaurus-plugin-typedoc-old/src/typedoc_plugin.ts
+++
@@ -1,8 +0,0 @@
-import { Application, Converter, Context } from 'typedoc';
-
-export function load(app: Application) {
-    app.converter.on(Converter.EVENT_BEGIN, (context: Context) => {
-        // rename `apify` to `Apify` on the docs HP
-        context.project.name = 'Apify';
-    });
-}
