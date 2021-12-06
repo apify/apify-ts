61630b1e6b5e16bc75bbca50af0687b2d4ac2ded
Vlad Frangu <kingdgrizzle@gmail.com>
2021-12-06 12:17:57 +0200

    chore: strictify stealth

--- packages/apify/src/typedefs.ts
+++ packages/apify/src/typedefs.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /** @ignore */
 export type Dictionary<T = unknown> = Record<PropertyKey, T>;
 
@@ -133,6 +134,10 @@ export interface ActorRun {
     output?: Dictionary | null;
 }
 
-export function entries<T extends Record<PropertyKey, unknown>>(obj: T) {
+export function entries<T extends {}>(obj: T) {
     return Object.entries(obj) as [keyof T, T[keyof T]][];
+}
+
+export function keys<T extends {}>(obj: T) {
+    return Object.keys(obj) as (keyof T)[];
 }
