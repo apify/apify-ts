b6057f92527a9ad6089cf4ee98ba7bd82b94fd26
Martin Adámek <banan23@gmail.com>
2022-01-14 10:51:10 +0100

    no more ts-ignore apart from stealth that will be removed anyway

--- packages/apify/src/index.ts
+++ packages/apify/src/index.ts
@@ -4,5 +4,5 @@ export default Apify;
 
 export * from './main';
 
-// @ts-ignore get around re-exporting warning for `utils`
+// @ts-expect-error get around re-exporting warning for `utils`
 export * from './exports';
