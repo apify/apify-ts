99e4dd97284435f77fbb8dcdfdf97e63317a901b
AndreyBykov <aabykov@gmail.com>
2022-01-25 19:20:47 +0100

    add cheerio-crawler e2e tests

---
+++ test/e2e/test-che-ssl-errors-disabled/test.mjs
@@ -0,0 +1,45 @@
+import { getStats, run, expect } from '../tools.mjs';
+
+await run(import.meta.url, 'cheerio-scraper', {
+    startUrls: [{
+        url: 'https://badssl.com/',
+        method: 'GET',
+        userData: { label: 'START' },
+    }],
+    keepUrlFragments: false,
+    pseudoUrls: [{
+        purl: 'https://[.+].badssl.com/',
+        method: 'GET',
+        userData: { label: 'DETAIL' },
+    }],
+    linkSelector: '.group a.bad',
+    pageFunction: async function pageFunction(context) {
+        const { request: { userData: { label } } } = context;
+
+        switch (label) {
+            case 'START': return handleStart(context);
+            case 'DETAIL': return handleDetail(context);
+        }
+
+        async function handleStart({ log, waitFor, $ }) {
+            log.info('Bad ssl page opened!');
+        }
+
+        async function handleDetail({ request, log, $ }) {
+            const { url } = request;
+            log.info(`Scraping ${url}`);
+            // Do some scraping.
+            const title = $('title').text();
+            return { url, title };
+        }
+    },
+    proxyConfiguration: { useApifyProxy: false },
+    proxyRotation: 'RECOMMENDED',
+    forceResponseEncoding: false,
+    ignoreSslErrors: false,
+    debugLog: false
+});
+
+const stats = await getStats(import.meta.url);
+expect(stats.requestsFinished, 23, 'All requests finished');
+process.exit(0);
