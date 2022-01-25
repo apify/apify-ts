4545949bef76093c24fc9ebefee68dd06380a6d8
AndreyBykov <aabykov@gmail.com>
2022-01-25 15:41:54 +0100

    add puppeteer-scraper default e2e test

---
+++ test/e2e/test-pup-default/test.mjs
@@ -0,0 +1,32 @@
+import { getStats, run, expect } from '../tools.mjs';
+
+await run(import.meta.url, 'puppeteer-scraper', {
+    startUrls: [{ url: 'https://apify.com' }],
+    pseudoUrls: [{ purl: 'https://apify.com[(/[\\w-]+)?]' }],
+    linkSelector: 'a',
+    keepUrlFragments: false,
+    pageFunction: async function pageFunction(context) {
+        const { page, request, log } = context;
+        const { url } = request;
+
+        const pageTitle = await page.title();
+        log.info(`URL: ${url} pageTitle: ${pageTitle}`);
+
+        return { url, pageTitle };
+    },
+    proxyConfiguration: { useApifyProxy: false },
+    proxyRotation: 'RECOMMENDED',
+    useChrome: false,
+    useStealth: false,
+    ignoreSslErrors: false,
+    ignoreCorsAndCsp: false,
+    downloadMedia: true,
+    downloadCss: true,
+    waitUntil: ['networkidle2'],
+    debugLog: false,
+    browserLog: false
+});
+
+const stats = await getStats(import.meta.url);
+expect(stats.requestsFinished, 83, 'All requests finished');
+process.exit(0);
