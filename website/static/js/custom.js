7eab500fbfa0f8cd14a745bf239316df0deefcb0
Martin Adámek <banan23@gmail.com>
2022-02-16 23:37:54 +0100

    docs: fix api docs version switching hack

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -1,11 +1,15 @@
 function load() {
     const versions = document.querySelectorAll('.navbar .dropdown ul a');
-    const types = ['/docs/next', '/docs'];
+    const types = ['/apify-ts/docs/next', '/apify-ts/docs'];
     let i = 0;
 
     for (const el of versions) {
         const match = el.href.match(/\/docs\/(\d+\.\d+(\.\d+)?)$/) || el.href.match(/\/docs\/(\d+\.\d+(\.\d+)?)/);
         const version = (types[i++] || match[0]).replace('/docs', '/api');
+
+        if (el.classList.contains('api-version-bound')) {
+            continue;
+        }
 
         el.addEventListener('click', (e) => {
             if (window.location.pathname.startsWith('/api')) {
@@ -13,15 +17,12 @@ function load() {
                 e.preventDefault();
             }
         });
+        el.classList.add('api-version-bound');
     }
 }
 
-const versions = document.querySelectorAll('.navbar .dropdown ul a');
-
-if (versions.length > 0) {
-    load();
-} else {
-    document.addEventListener('DOMContentLoaded', () => {
-        setTimeout(load, 500);
-    });
-}
+setInterval(() => {
+    if (document.querySelectorAll('.navbar .dropdown ul a').length > 0) {
+        load();
+    }
+}, 500);
