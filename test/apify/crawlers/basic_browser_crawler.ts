a6241dbd0146515c5ae13a77ebe87561ecaa8a7d
Vlad Frangu <kingdgrizzle@gmail.com>
2022-04-26 02:01:48 +0300

    chore: fix test?

--- test/apify/crawlers/basic_browser_crawler.ts
+++ test/apify/crawlers/basic_browser_crawler.ts
@@ -6,4 +6,8 @@ export class BrowserCrawlerTest extends BrowserCrawler<{ browserPlugins: [Puppet
     constructor(options: Partial<PuppeteerCrawlerOptions> = {}) {
         super(options as any);
     }
+
+    protected override _applyCookies(ctx: PuppeteerCrawlContext, preHooksCookies: string, postHookCookies: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
 }
