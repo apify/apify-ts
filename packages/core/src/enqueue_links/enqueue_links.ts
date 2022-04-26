4406dd4ba2251baee3486a6ac14f0a58e8c26f4f
Martin Adámek <banan23@gmail.com>
2022-04-26 23:54:14 +0200

    fix: allow falsy values in `transformRequestFunction` type

--- packages/core/src/enqueue_links/enqueue_links.ts
+++ packages/core/src/enqueue_links/enqueue_links.ts
@@ -15,6 +15,7 @@ import {
 } from './shared';
 import { RequestQueue } from '../storages/request_queue';
 import { BatchAddRequestsResult } from '../storages/storage';
+import { RequestOptions } from '../request';
 
 export interface EnqueueLinksOptions {
     /** Limit the count of actually enqueued URLs to this number. Useful for testing across the entire crawling scope. */
@@ -254,7 +255,7 @@ export async function enqueueLinks(options: EnqueueLinksOptions): Promise<BatchA
     let requestOptions = createRequestOptions(urls);
 
     if (transformRequestFunction) {
-        requestOptions = requestOptions.map((request) => transformRequestFunction(request)).filter((r) => !!r);
+        requestOptions = requestOptions.map((request) => transformRequestFunction(request)).filter((r) => !!r) as RequestOptions[];
     }
 
     let requests = createRequests(requestOptions, urlPatternObjects);
