493920907c93e53b0fa64fe7fc9d5b104268c2e7
AndreyBykov <aabykov@gmail.com>
2022-05-15 00:12:53 +0200

    rename test-cheerio-store-pagination -> test-cheerio-max-requests

--- test/e2e/test-cheerio-max-requests/test.mjs
+++ test/e2e/test-cheerio-max-requests/test.mjs
@@ -1,5 +1,5 @@
 import { initialize, expect, validateDataset, getActorTestDir, runActor } from '../tools.mjs';
-// TODO: I would delete this test. It just adds initial requests, enqueues more links and pushes data to dataset.
+
 const testActorDirname = getActorTestDir(import.meta.url);
 await initialize(testActorDirname);
 
