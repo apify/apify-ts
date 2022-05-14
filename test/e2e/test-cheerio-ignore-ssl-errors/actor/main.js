f309299fb6fe25958d62ba35f65d4cd5639286eb
AndreyBykov <aabykov@gmail.com>
2022-05-14 22:57:57 +0200

    update test-cheerio-ignore-ssl-errors

---
+++ test/e2e/test-cheerio-ignore-ssl-errors/actor/main.js
@@ -0,0 +1,25 @@
+import { Actor } from 'apify';
+import { CheerioCrawler } from '@crawlee/cheerio';
+
+await Actor.main(async () => {
+    const requestHandler = async ({ $, enqueueLinks, request, log }) => {
+        const { userData: { label } } = request;
+
+        if (label === 'START') {
+            log.info('Bad ssl page opened!');
+            await enqueueLinks({
+                globs: [{ glob: 'https://*.badssl.com/', userData: { label: 'DETAIL' } }],
+                selector: '.group a.bad',
+            });
+        } else if (label === 'DETAIL') {
+            const { url } = request;
+            log.info(`Scraping ${url}`);
+            const title = $('title').text();
+            await Actor.pushData({ url, title });
+        }
+    };
+
+    const crawler = new CheerioCrawler({ requestHandler, ignoreSslErrors: true });
+    await crawler.addRequests([{ url: 'https://badssl.com', userData: { label: 'START' } }]);
+    await crawler.run();
+});
---
+++ test/e2e/test-cheerio-ignore-ssl-errors/test.mjs
@@ -0,0 +1,14 @@
+import { initialize, getActorTestDir, runActor, expect, validateDataset } from '../tools.mjs';
+
+const testActorDirname = getActorTestDir(import.meta.url);
+await initialize(testActorDirname);
+
+const { stats, datasetItems } = await runActor(testActorDirname, true);
+
+expect(stats.requestsFinished > 20, 'All requests finished');
+
+expect(datasetItems.length > 20, 'Minimum number of dataset items');
+await new Promise((resolve) => setTimeout(resolve, 10));
+expect(validateDataset(datasetItems, ['url', 'title']), 'Dataset items validation');
+
+process.exit(0);
