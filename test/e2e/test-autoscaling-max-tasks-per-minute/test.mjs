844e5592ffc8afed5430b81c0c291486e3cdb8db
AndreyBykov <aabykov@gmail.com>
2022-05-15 16:57:26 +0200

    add 'deep-equal' to dependencies for respective tests

--- test/e2e/test-autoscaling-max-tasks-per-minute/test.mjs
+++ test/e2e/test-autoscaling-max-tasks-per-minute/test.mjs
@@ -8,7 +8,7 @@ const { stats } = await runActor(testActorDirname);
 expect(stats.requestsFinished === 2, 'All requests finished');
 await new Promise((resolve) => setTimeout(resolve, 10));
 expect(
-    stats.crawlerRuntimeMillis > 60_000 && stats.crawlerRuntimeMillis < 61_000,
+    stats.crawlerRuntimeMillis > 60_000 && stats.crawlerRuntimeMillis < 65_000,
     `Ran one task per minute, took ~1 minute to complete, but no more than that (${stats.crawlerRuntimeMillis}ms)`,
 );
 
