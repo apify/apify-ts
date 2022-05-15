6b1c429492eb59cb675f941e8f34a7d612a4412a
AndreyBykov <aabykov@gmail.com>
2022-05-15 03:18:41 +0200

    update test-puppeteer-throw-on-ssl-errors

---
+++ test/e2e/test-puppeteer-throw-on-ssl-errors/actor/main.js
@@ -0,0 +1,30 @@
+import { Actor } from 'apify';
+import { PuppeteerCrawler } from '@crawlee/puppeteer';
+
+await Actor.main(async () => {
+    const preNavigationHooks = [(_ctx, goToOptions) => {
+        goToOptions.waitUntil = ['networkidle2'];
+    }];
+    // This is the default
+    const launchContext = { launchOptions: { ignoreHTTPSErrors: false } };
+
+    const requestHandler = async ({ page, enqueueLinks, request, log }) => {
+        const { url, userData: { label } } = request;
+
+        if (label === 'START') {
+            log.info('Bad ssl page opened!');
+            await enqueueLinks({
+                globs: [{ glob: 'https://*.badssl.com/', userData: { label: 'DETAIL' } }],
+                selector: '.group a.bad',
+            });
+        } else if (label === 'DETAIL') {
+            log.info(`Scraping ${url}`);
+            const title = await page.title();
+            await Actor.pushData({ url, title });
+        }
+    };
+
+    const crawler = new PuppeteerCrawler({ requestHandler, preNavigationHooks, launchContext });
+    await crawler.addRequests([{ url: 'https://badssl.com', userData: { label: 'START' } }]);
+    await crawler.run();
+});
--- test/e2e/test-puppeteer-throw-on-ssl-errors/test.mjs
+++ test/e2e/test-puppeteer-throw-on-ssl-errors/test.mjs
@@ -5,11 +5,11 @@ await initialize(testActorDirname);
 
 const { stats, datasetItems } = await runActor(testActorDirname, true);
 
-expect(stats.requestsFinished > 5 && stats.requestsFinished < 10, 'All requests finished');
+expect(stats.requestsFinished >= 5, 'All requests finished');
 await new Promise((resolve) => setTimeout(resolve, 10));
 expect(stats.requestsFailed > 20 && stats.requestsFailed < 30, 'Number of failed requests');
 
-expect(datasetItems.length > 5 && datasetItems.length < 10, 'Number of dataset items');
+expect(datasetItems.length >= 5 && datasetItems.length < 10, 'Number of dataset items');
 await new Promise((resolve) => setTimeout(resolve, 10));
 expect(validateDataset(datasetItems, ['url', 'title']), 'Dataset items validation');
 
