22e08c7eb7c9afface7c1c70fea6d81c559ed049
Martin Adámek <banan23@gmail.com>
2022-05-23 22:23:40 +0200

    chore: remove `@crawlee/storage` (#110)

--- packages/storage/src/emulators/batch_add_requests/unprocessed_request.ts
+++
@@ -1,4 +0,0 @@
-export class UnprocessedRequest {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(public uniqueKey: string, public url: string, public method?: string) {}
-}
