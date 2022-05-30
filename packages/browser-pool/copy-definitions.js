eb2bc73699a0df7a4f290fda24fe5f4ca63d17be
Martin Adámek <banan23@gmail.com>
2022-05-30 13:23:23 +0200

    chore: copy fingerprint generator types during publish

--- packages/browser-pool/copy-definitions.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-fs.copyFileSync(
-    path.join('src', 'puppeteer', 'puppeteer-proxy-per-page.d.ts'),
-    path.join('dist', 'puppeteer', 'puppeteer-proxy-per-page.d.ts'),
-);
---
+++ packages/browser-pool/copy-definitions.mjs
@@ -0,0 +1,14 @@
+import { copyFileSync } from 'fs';
+import { join } from 'path';
+
+const files = [
+    'puppeteer/puppeteer-proxy-per-page.d.ts',
+    'types.d.ts',
+];
+
+for (const file of files) {
+    copyFileSync(
+        join('src', file),
+        join('dist', file),
+    );
+}
