54de1e209105357c913f0c3e68bd12019055616c
AndreyBykov <aabykov@gmail.com>
2022-05-31 18:24:59 +0200

    use 16384 memory for browser based tests on the platform

--- test/e2e/puppeteer-store-pagination/test.mjs
+++ test/e2e/puppeteer-store-pagination/test.mjs
@@ -3,7 +3,7 @@ import { initialize, getActorTestDir, runActor, expect, validateDataset } from '
 const testActorDirname = getActorTestDir(import.meta.url);
 await initialize(testActorDirname);
 
-const { stats, datasetItems } = await runActor(testActorDirname);
+const { stats, datasetItems } = await runActor(testActorDirname, 16384);
 
 await expect(stats.requestsFinished > 10, 'All requests finished');
 await expect(datasetItems.length > 5 && datasetItems.length < 15, 'Number of dataset items');
