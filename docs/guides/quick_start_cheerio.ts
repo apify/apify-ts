edcb1f6f88038ab16cc63a13ee5c256f7878d47f
AndreyBykov <aabykov@gmail.com>
2022-06-08 17:26:54 +0200

    partially update quick_start (waiting for CLI)

---
+++ docs/guides/quick_start_cheerio.ts
@@ -0,0 +1,20 @@
+import { CheerioCrawler, Dataset } from "@crawlee/cheerio";
+
+const crawler = new CheerioCrawler({
+    async requestHandler({ request, $, enqueueLinks }) {
+        const { url } = request;
+        // Extract HTML title of the page.
+        const title = $('title').text();
+        console.log(`Title of ${url}: ${title}`);
+        // Add URLs that match the provided pattern.
+        await enqueueLinks({
+            globs: ['https://www.iana.org/*'],
+        });
+        // Save extracted data to dataset.
+        await Dataset.pushData({ url, title });
+    },
+});
+// Choose the first URL to open.
+await crawler.addRequests([{ url: 'https://www.iana.org/' }]);
+// Start the crawler.
+await crawler.run();
---
+++ docs/guides/quick_start_puppeteer.ts
@@ -0,0 +1,20 @@
+import { PuppeteerCrawler, Dataset } from "@crawlee/puppeteer";
+
+const crawler = new PuppeteerCrawler({
+    async requestHandler({ request, page, enqueueLinks }) {
+        const { url } = request;
+        // Extract HTML title of the page.
+        const title = await page.title();
+        console.log(`Title of ${request.url}: ${title}`);
+        // Add URLs that match the provided pattern.
+        await enqueueLinks({
+            globs: ['https://www.iana.org/*'],
+        });
+        // Save extracted data to dataset.
+        await Dataset.pushData({ url, title });
+    },
+});
+// Choose the first URL to open.
+await crawler.addRequests([{ url: 'https://www.iana.org/' }]);
+// Start the crawler.
+await crawler.run();
