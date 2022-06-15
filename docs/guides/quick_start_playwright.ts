ee16dd4d1dae07a693095fe5f8a03628cfb9a93f
AndreyBykov <aabykov@gmail.com>
2022-06-15 15:35:56 +0200

    add some spaces to quick start guides

--- docs/guides/quick_start_playwright.ts
+++ docs/guides/quick_start_playwright.ts
@@ -3,18 +3,23 @@ import { PlaywrightCrawler, Dataset } from "@crawlee/playwright";
 const crawler = new PlaywrightCrawler({
     async requestHandler({ request, page, enqueueLinks }) {
         const { url } = request;
+
         // Extract HTML title of the page.
         const title = await page.title();
         console.log(`Title of ${url}: ${title}`);
+
         // Add URLs that match the provided pattern.
         await enqueueLinks({
             globs: ['https://www.iana.org/*'],
         });
+
         // Save extracted data to dataset.
         await Dataset.pushData({ url, title });
     },
 });
+
 // Choose the first URL to open.
 await crawler.addRequests([{ url: 'https://www.iana.org/' }]);
+
 // Start the crawler.
 await crawler.run();
--- docs/guides/quick_start_puppeteer.ts
+++ docs/guides/quick_start_puppeteer.ts
@@ -3,18 +3,23 @@ import { PuppeteerCrawler, Dataset } from "@crawlee/puppeteer";
 const crawler = new PuppeteerCrawler({
     async requestHandler({ request, page, enqueueLinks }) {
         const { url } = request;
+
         // Extract HTML title of the page.
         const title = await page.title();
-        console.log(`Title of ${request.url}: ${title}`);
+        console.log(`Title of ${url}: ${title}`);
+
         // Add URLs that match the provided pattern.
         await enqueueLinks({
             globs: ['https://www.iana.org/*'],
         });
+
         // Save extracted data to dataset.
         await Dataset.pushData({ url, title });
     },
 });
+
 // Choose the first URL to open.
 await crawler.addRequests([{ url: 'https://www.iana.org/' }]);
+
 // Start the crawler.
 await crawler.run();
