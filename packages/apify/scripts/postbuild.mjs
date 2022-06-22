f717466a0dd1e820b03711d8731e89a48fdf0693
Vlad Frangu <kingdgrizzle@gmail.com>
2022-06-23 00:03:37 +0300

    chore: small reverts and small fixes

---
+++ packages/apify/scripts/postbuild.mjs
@@ -0,0 +1,17 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+const indexDTsFile = new URL('../dist/index.d.ts', import.meta.url);
+const ident = ' '.repeat(4);
+
+const currentContent = (await readFile(indexDTsFile, 'utf8')).split('\n');
+
+currentContent.push(
+	`// Augment the crawlee KeyValueStore with the new available method`,
+`declare module '@crawlee/core' {
+    interface KeyValueStore {
+        getPublicUrl(key: string): string;
+    }
+}
+`);
+
+await writeFile(indexDTsFile, currentContent.join('\n'));
--- packages/apify/src/key_value_store.ts
+++ packages/apify/src/key_value_store.ts
@@ -1,12 +1,7 @@
 import { KeyValueStore } from '@crawlee/core';
 
 // Augment the key value store with the Apify specific methods.
+// @ts-expect-error We cannot augment this in the monorepo, so we do it in a post build script
 KeyValueStore.prototype.getPublicUrl = function (this: KeyValueStore, key: string) {
     return `https://api.apify.com/v2/key-value-stores/${this.id}/records/${key}`;
 };
-
-declare module '@crawlee/core' {
-    interface KeyValueStore {
-        getPublicUrl(key: string): string;
-    }
-}
