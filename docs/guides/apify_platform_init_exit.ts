07a0576e83722aa913508d77400c2e1cf44c6057
AndreyBykov <aabykov@gmail.com>
2022-07-07 01:49:43 +0200

    update apify_platform guide

---
+++ docs/guides/apify_platform_init_exit.ts
@@ -0,0 +1,33 @@
+import { Actor } from 'apify'
+import { CheerioCrawler } from "@crawlee/cheerio";
+
+await Actor.init();
+
+const proxyConfiguration = await Actor.createProxyConfiguration();
+
+const crawler = new CheerioCrawler({
+    proxyConfiguration,
+    async requestHandler({ request, $, enqueueLinks }) {
+        const { url } = request;
+
+        // Extract HTML title of the page.
+        const title = $('title').text();
+        console.log(`Title of ${url}: ${title}`);
+
+        // Add URLs that match the provided pattern.
+        await enqueueLinks({
+            globs: ['https://www.iana.org/*'],
+        });
+
+        // Save extracted data to dataset.
+        await Actor.pushData({ url, title });
+    },
+});
+
+// Choose the first URL to open.
+await crawler.addRequests([{ url: 'https://www.iana.org/' }]);
+
+// Start the crawler.
+await crawler.run();
+
+await Actor.exit();
