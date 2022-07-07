d0198bf130014d72655cc21ac699b7a31fe8c9c2
AndreyBykov <aabykov@gmail.com>
2022-07-08 01:21:20 +0200

    few nits for proxy_management

--- docs/guides/proxy_management_inspection_cheerio.ts
+++ docs/guides/proxy_management_inspection_cheerio.ts
@@ -1,6 +1,7 @@
 import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
 
 const proxyConfiguration = new ProxyConfiguration({ /* opts */ });
+
 const crawler = new CheerioCrawler({
     proxyConfiguration,
     async requestHandler({ proxyInfo }) {
