d37bfebb30f17397ffc7350f05b79204f64f677f
Vlad Frangu <kingdgrizzle@gmail.com>
2022-07-08 10:03:52 +0300

    chore: switch to eslint

--- packages/browser-pool/src/browser-pool.ts
+++ packages/browser-pool/src/browser-pool.ts
@@ -336,12 +336,8 @@ export class BrowserPool<
                 const firstPluginName = firstPluginConstructor.name;
                 const providedPluginName = (providedPlugin as BrowserPlugin).constructor.name;
 
-                throw new Error(
-                    [
-                        `Browser plugin at index ${i} (${providedPluginName}) is not an instance of the same plugin`,
-                        `as the first plugin provided (${firstPluginName}).`,
-                    ].join(' '),
-                );
+                // eslint-disable-next-line max-len
+                throw new Error(`Browser plugin at index ${i} (${providedPluginName}) is not an instance of the same plugin as the first plugin provided (${firstPluginName}).`);
             }
         }
 
