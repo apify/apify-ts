8b9b2f9c3c1e44abe5308111af2055529d39be06
AndreyBykov <aabykov@gmail.com>
2022-07-12 02:43:45 +0200

    fix some typos + few nit in got-scraping guide

--- website/sidebars.js
+++ website/sidebars.js
@@ -20,10 +20,10 @@ module.exports = {
                 'guides/session-management',
                 'guides/scaling-crawlers',
                 'guides/avoid-blocking',
+                'guides/got-scraping',
                 'guides/typescript-project',
                 'guides/docker-images',
                 'guides/apify-platform',
-                'guides/got-scraping',
             ],
         },
         {
